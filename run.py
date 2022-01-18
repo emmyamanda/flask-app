538eed289650fbee76c30e1020a131e789410326
emmyamanda <emmysignal95@hotmail.com>
2022-01-18 19:03:01 +0000

    Added files from bootstrap etc

---
+++ run.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+"""
+Do not have debug=True when doing a project
+only when you try your project but not submitting
+"""
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
